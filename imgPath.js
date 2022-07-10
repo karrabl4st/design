9660a2516f67a55c884cf78032507a881e05533c
karrabl4st <karraisepic@gmail.com>
2022-07-10 02:05:53 -0500

    put all on html file

--- imgPath.js
+++
@@ -1,7 +0,0 @@
-const style = getComputedStyle(document.documentElement);
-const path = '/Users/jls200015/Library/CloudStorage/Box-Box/images';
-const imgs = document.querySelectorAll('img');
-imgs.forEach(img => {
-    let fileName = img.id;
-    img.src = path + '/' + fileName + '.png';
-});
